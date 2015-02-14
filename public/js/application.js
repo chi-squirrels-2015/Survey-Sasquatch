8265c5241f96d6d1cda44d41e18cc83e7c382043
Ian Agne <ian.d.agne@gmail.com>
2015-02-14 13:31:51 -0600

    add jquery stuff

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.login').on('click', function(event) {
+    event.preventDefault();
+    $('#login_form').css('visibility', 'visible')
+    $('#signup_form').css('visibility', 'hidden')
+  });
+  $('.signup').on('click', function() {
+    event.preventDefault();
+    $('#signup_form').css('visibility', 'visible')
+    $('#login_form').css('visibility', 'hidden')
+  });
 });
