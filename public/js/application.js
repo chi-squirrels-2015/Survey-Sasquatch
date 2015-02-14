b20d57a10da0039b0885a60aaa4741fc498b3446
Ian Agne <ian.d.agne@gmail.com>
2015-02-13 19:22:17 -0600

    add ajax 'add question' button

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#question_block").on("click", ".questions", function(event){
+    event.preventDefault();
+    $("#question_block")
+
+    var request = $.ajax({
+      url: "/questions/new",
+      type: "get"
+    })
+
+    request.done(function(response){
+      $("#question_block").append(response);
+    });
+  });
+
 });
